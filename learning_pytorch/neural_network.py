3b9eb007a32a645a7d641f5a7de57029d3268bec
Ian Horsman <ianphorsman@gmail.com>
2018-03-18 17:58:00 -0400

    parent class for models with helper functions

---
+++ learning_pytorch/neural_network.py
@@ -0,0 +1,15 @@
+import torch
+
+class TorchNeuralNetwork(object):
+
+    def __init__(
+        self,
+        model_filename='../data/model',
+    ):
+        self.model_filename = model_filename
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
