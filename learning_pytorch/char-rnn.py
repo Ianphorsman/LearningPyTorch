ab0c0befa9b3920daed59cbdf49ff824792dff7c
Ian Horsman <ianphorsman@gmail.com>
2018-03-18 17:57:39 -0400

    CharRNN started for first pytorch tutorial that uses a char-rnn and lstm cells to generate names

---
+++ learning_pytorch/char-rnn.py
@@ -0,0 +1,11 @@
+import torch
+from learning_pytorch.neural_network import TorchNeuralNetwork
+
+class CharRNN(TorchNeuralNetwork):
+
+    def __init__(
+        self
+    ):
+        super().__init__()
+
+
